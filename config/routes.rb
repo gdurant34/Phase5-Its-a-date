513a1d2d1f4c6ede8cba87072b74fdb5bf1c4974
Grace Durant <gdurant34@gmail.com>
2022-12-15 20:45:02 -0800

    setup resources, ran migrations, and seeded db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   
+  resources :comments
+  resources :dayt_activities
+  resources :user_relationships
+  resources :activities
+  resources :dayts
+  resources :relationships
+  resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
